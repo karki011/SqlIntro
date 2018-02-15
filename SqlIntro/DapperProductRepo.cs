d5581b10ef2c7b6e84e6c35b7afc17853bd60a8d
Subash Karki <karki011@gmail.com>
2018-02-15 13:34:12 -0600

    updated and added all the CRUD method

--- SqlIntro/DapperProductRepo.cs
+++ SqlIntro/DapperProductRepo.cs
@@ -1,24 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using Dapper;
-
 
 namespace SqlIntro
 {
-    class DapperProductRepo
+    class DapperProductRepo : IProductRepository
     {
-       
-        //SELECT all products 
-      
-        // DELETE a product with a particular ProductId
-        // UPDATE a product's name with a particular ProductId
-        //INSERT a new product
-    }
+        private readonly IDbConnection _conn;
 
+        public DapperProductRepo(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        /// <summary>
+        /// Reads all the products from the products table
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Product> GetProducts()
+        {
+            return _conn.Query<Product>("SELECT name FROM Product;");
+
+        }
+        /// <summary>
+        /// Deletes a Product from the database
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeleteProduct(int id)
+        {
+            _conn.Execute("Delete FROM Product WHERE ProductID =@id", new { id });
+        }
+        /// <summary>
+        /// Updates the Product in the database
+        /// </summary>
+        /// <param name="prod"></param>
+        public void UpdateProduct(Product prod)
+        {
+            _conn.Execute("UPDATE product SET name = @name WHERE ProductId = @id", new { name = prod.Name, id = prod.Id });
+        }
+        /// <summary>
+        /// Inserts a new Product into the database
+        /// </summary>
+        /// <param name="prod"></param>
+        public void InsertProduct(Product prod)
+        {
+            _conn.Execute("INSERT into product (name) values(@name)", new { name = prod.Name });
+        }
+    }
 }
 
