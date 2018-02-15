1d775fa536435d5f26a757e27acf20b41c4beb21
Subash Karki <karki011@gmail.com>
2018-02-15 08:29:10 -0600

    modified git ignore

--- SqlIntro/DapperProductRepo.cs
+++ SqlIntro/DapperProductRepo.cs
@@ -5,23 +5,16 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using Dapper;
 
 
 namespace SqlIntro
 {
     class DapperProductRepo
     {
-        private readonly string _connectionString;
-        public DapperProductRepo(string connectionString)
-        {
-            _connectionString = connectionString;
-        }
+       
         //SELECT all products 
-        /*public IEnumerable<Product> GetProducts()
-        {
-            return
-        }*/
-
+      
         // DELETE a product with a particular ProductId
         // UPDATE a product's name with a particular ProductId
         //INSERT a new product
